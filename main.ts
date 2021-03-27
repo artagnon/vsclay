fccd34e994924898ca34dab815886bde2bfb195a
Ramkumar Ramachandra <r@artagnon.com>
2021-03-27 21:12:14 +0100

    [main] Get the Hoverer working

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 import {
   Position,
   CompletionItemProvider,
+  HoverProvider,
   CompletionItem,
+  Hover,
   CancellationToken,
   TextDocument,
   CompletionContext,
@@ -10,11 +12,22 @@ import {
 import * as vscode from 'vscode';
 import { readFileSync } from 'fs';
 
-class Completer implements CompletionItemProvider {
-  private extensionRoot: string;
-
+abstract class Extension {
+  protected extensionRoot: string;
+  protected symbols: Object;
+  protected commands: Object;
+  protected allObjs: Object;
   constructor(extensionRoot: string) {
     this.extensionRoot = extensionRoot;
+    this.symbols = JSON.parse(readFileSync(`${this.extensionRoot}/data/symbols.json`, { encoding: 'utf8' }));
+    this.commands = JSON.parse(readFileSync(`${this.extensionRoot}/data/commands.json`, { encoding: 'utf8' }));
+    this.allObjs = { ...this.symbols, ...this.commands };
+  }
+}
+
+class Completer extends Extension implements CompletionItemProvider {
+  constructor(extensionRoot: string) {
+    super(extensionRoot);
   }
   provideCompletionItems(
     _doc: TextDocument,
@@ -22,10 +35,25 @@ class Completer implements CompletionItemProvider {
     _tok: CancellationToken,
     _ctx: CompletionContext
   ): CompletionItem[] {
-    const symbols = JSON.parse(readFileSync(`${this.extensionRoot}/data/symbols.json`, { encoding: 'utf8' }));
-    const commands = JSON.parse(readFileSync(`${this.extensionRoot}/data/commands.json`, { encoding: 'utf8' }));
-    const keys = Object.keys(symbols).concat(Object.keys(commands));
+    const keys = Object.keys(this.symbols).concat(Object.keys(this.commands));
     return keys.map((k) => new CompletionItem(k));
+  }
+}
+
+class Hoverer extends Extension implements HoverProvider {
+  constructor(extensionRoot: string) {
+    super(extensionRoot);
+  }
+  provideHover(document: TextDocument, position: Position, _: CancellationToken): Hover | null {
+    const tok = document.getText(document.getWordRangeAtPosition(position));
+    const matchingEntries = Object.entries(this.allObjs).filter(([k, _]) => k == tok);
+    if (matchingEntries.length == 0) return null;
+    const match: Map<string, string> = new Map(Object.entries(matchingEntries[0][1]));
+    const detail = match.get('detail');
+    const documentation = match.get('documentation');
+    if (!detail && !documentation) return null;
+    const doc = (detail ? detail?.toString() + '\n--\n' : '') + (documentation ? documentation?.toString() : '');
+    return new Hover(doc);
   }
 }
 
@@ -36,5 +64,8 @@ export function activate(context: ExtensionContext) {
       new Completer(context.extensionPath),
       '\\'
     )
+  );
+  context.subscriptions.push(
+    vscode.languages.registerHoverProvider({ scheme: 'file', language: 'claytext' }, new Hoverer(context.extensionPath))
   );
 }
