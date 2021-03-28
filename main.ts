b73087d334bb346f3d130e23e3bcd52a6a82c3b7
Ramkumar Ramachandra <r@artagnon.com>
2021-03-28 12:03:23 +0200

    [data] Clean up and sort, and add official list

--- main.ts
+++ main.ts
@@ -20,27 +20,18 @@ abstract class Extension {
   protected allObjs: Object;
   constructor(extensionRoot: string) {
     this.extensionRoot = extensionRoot;
-    this.symbols = JSON.parse(
-      readFileSync(`${this.extensionRoot}/data/symbols.json`, {
-        encoding: "utf8",
-      })
-    );
-    this.commands = JSON.parse(
-      readFileSync(`${this.extensionRoot}/data/commands.json`, {
-        encoding: "utf8",
-      })
-    );
+    this.symbols = JSON.parse(readFileSync(`${this.extensionRoot}/data/symbols.json`, { encoding: "utf8" }));
+    this.commands = JSON.parse(readFileSync(`${this.extensionRoot}/data/commands.json`, { encoding: "utf8" }));
     this.allObjs = { ...this.symbols, ...this.commands };
   }
 }
 
-interface SymbolEntry {
-  command: string;
+interface DocumentEntry {
   detail?: string;
   documentation?: string;
 }
 
-interface CommandEntry extends SymbolEntry {
+interface CommandEntry extends DocumentEntry {
   snippet: string;
 }
 
@@ -48,33 +39,21 @@ function snippetString(obj: CommandEntry): SnippetString {
   return new SnippetString(obj.snippet);
 }
 
-function containsDoc(obj: any): obj is SymbolEntry {
+function containsDoc(obj: any): obj is DocumentEntry {
   return typeof obj.detail == "string" || typeof obj.documentation == "string";
 }
 
-function docString(obj: SymbolEntry) {
-  return (
-    (obj.detail ? obj.detail?.toString() + "\n--\n" : "") +
-    (obj.documentation ? obj.documentation?.toString() : "")
-  );
+function docString(obj: DocumentEntry) {
+  return (obj.detail ? obj.detail?.toString() + "\n--\n" : "") + (obj.documentation ? obj.documentation?.toString() : "");
 }
 
 class Completer extends Extension implements CompletionItemProvider {
   constructor(extensionRoot: string) {
     super(extensionRoot);
   }
-  provideCompletionItems(
-    _doc: TextDocument,
-    _pos: Position,
-    _tok: CancellationToken,
-    _ctx: CompletionContext
-  ): CompletionItem[] {
-    const snippets = new Map(
-      Object.entries(this.commands).map(([k, v]) => [k, snippetString(v)])
-    );
-    const suggestions = Object.keys(this.allObjs).map(
-      (k) => new CompletionItem(k, vscode.CompletionItemKind.Variable)
-    );
+  provideCompletionItems(_doc: TextDocument, _pos: Position, _tok: CancellationToken, _ctx: CompletionContext): CompletionItem[] {
+    const snippets = new Map(Object.entries(this.commands).map(([k, v]) => [k, snippetString(v)]));
+    const suggestions = Object.keys(this.allObjs).map((k) => new CompletionItem(k, vscode.CompletionItemKind.Variable));
     suggestions.forEach((i) => (i.insertText = snippets.get(i.label)));
     return suggestions;
   }
@@ -84,15 +63,9 @@ class Hoverer extends Extension implements HoverProvider {
   constructor(extensionRoot: string) {
     super(extensionRoot);
   }
-  provideHover(
-    document: TextDocument,
-    position: Position,
-    _: CancellationToken
-  ): Hover | null {
+  provideHover(document: TextDocument, position: Position, _: CancellationToken): Hover | null {
     const tok = document.getText(document.getWordRangeAtPosition(position));
-    const matchingEntries = Object.entries(this.allObjs).filter(
-      ([k, _]) => k == tok
-    );
+    const matchingEntries = Object.entries(this.allObjs).filter(([k, _]) => k == tok);
     if (matchingEntries.length == 0) return null;
     const match = matchingEntries[0][1];
     return containsDoc(match) ? new Hover(docString(match)) : null;
@@ -101,16 +74,9 @@ class Hoverer extends Extension implements HoverProvider {
 
 export function activate(context: ExtensionContext) {
   context.subscriptions.push(
-    vscode.languages.registerCompletionItemProvider(
-      { scheme: "file", language: "claytext" },
-      new Completer(context.extensionPath),
-      "\\"
-    )
+    vscode.languages.registerCompletionItemProvider({ scheme: "file", language: "claytext" }, new Completer(context.extensionPath), "\\")
   );
   context.subscriptions.push(
-    vscode.languages.registerHoverProvider(
-      { scheme: "file", language: "claytext" },
-      new Hoverer(context.extensionPath)
-    )
+    vscode.languages.registerHoverProvider({ scheme: "file", language: "claytext" }, new Hoverer(context.extensionPath))
   );
 }
